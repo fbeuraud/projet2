2ce3ff4da926fd87e0715d390f6c2761e7eeccdc
francois.beuraud <francois.beuraud@gmail.com>
2016-11-22 08:58:01 -0500

    commit apres change Gemfile

--- test/helpers/students_helper_test.rb
+++ test/helpers/students_helper_test.rb
@@ -1,4 +1,12 @@
 require 'test_helper'
 
 class StudentsHelperTest < ActionView::TestCase
+  test "minor student" do
+    joe = Student.find_by_name("Joe")
+    assert_equal joe.major?, true
+  end
+  test "major student" do
+    bill = Student.find_by_name("Bill")
+    assert_equal bill.major?, false
+  end
 end
